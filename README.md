668367eca0db9b7b6a56c079616476338706f941
권은서 <kwonenseo@gmail.com>
2022-04-24 13:30:34 +0900

    docs: Modified README.md

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # wanted_pre_onboarding
-### 지원자 : 권은서
+### About Me
+지원자 : 권은서 
+이메일 : kwonenseo@gmail.com
+
+### Stacks
+- React.js
+- styled-components
+
+### 수행한 사전 과제
+- Toggle.js
+- Tab.js
+- Slider.js
--- src/App.js
+++ src/App.js
@@ -3,15 +3,30 @@ import Input from "./components/Input";
 import Slider from "./components/Slider";
 import Tab from "./components/Tab";
 import Toggle from "./components/Toggle";
+import styled from "styled-components";
+
+const Div = styled.div`
+  margin: 50px;
+`;
 
 function App() {
   return (
     <>
-    <Toggle />
-    <Tab />
-    <Slider />
-    <Input />
-    <Dropdown />
+    <Div>
+      <Toggle />
+    </Div>
+    <Div>
+      <Tab />
+    </Div>
+    <Div>
+      <Slider />
+    </Div>
+    <Div>
+      <Input />
+    </Div>
+    <Div>
+      <Dropdown />
+    </Div>
     </>
   );
 }
